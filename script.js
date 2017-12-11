e47e74a7246480865d6968c9eb78a43661d4fb20
Dan Strong <danielstrong@mac.com>
2017-12-11 11:26:44 -0900

    Update script.js

--- script.js
+++ script.js
@@ -9,43 +9,33 @@ container.setAttribute('class', 'container');
 app.appendChild(logo);
 app.appendChild(container);
 
-// create a request variable and assign a new XMLHttpRequest object to it
 var request = new XMLHttpRequest();
-// open a new connection using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.onload = function () {
 
-request.onload = function() {
-  
-  // begin accessing JSON data here
-  var data = JSON.parse(this.reponse);
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
-      // create a div with a card class
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
-      
-      // create an H1 and set the text content to the film title
+
       const h1 = document.createElement('h1');
       h1.textContent = movie.title;
-      
-      // create a p and set text content to film's description
+
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300); // limit to 300 chars
-      p.textContent = `$(movie.description)...`; // end with an ellipses
-      
-      // append the cards to the container element
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
       container.appendChild(card);
-      
-      // each card will contain an h1 and a p
       card.appendChild(h1);
-      card.appendChild(p); 
+      card.appendChild(p);
     });
   } else {
     const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah! It's not working`;
+    errorMessage.textContent = `Gah, it's not working!`;
     app.appendChild(errorMessage);
   }
 }
 
 request.send();
-
